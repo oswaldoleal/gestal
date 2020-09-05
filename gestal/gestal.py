f49943fc920447f701846adca680f8161787aac9
Oswaldo Leal <ojlr.1998@gmail.com>
2020-09-05 09:54:34 -0400

    Showing interface in the top level executable

--- gestal/gestal.py
+++ gestal/gestal.py
@@ -1,4 +1,9 @@
-# Main / Top level executable
+# Main / Top level executable TODO
 
 import gi
 gi.require_version('Gdk', '3.0')
+from gui.application import Application
+
+
+app = Application('org.gnome.gestal')
+app.run()
