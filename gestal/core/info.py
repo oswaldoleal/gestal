2995aae2bfe945ff49bd7813e4c2d3de75ce97b7
Oswaldo Leal <ojlr.1998@gmail.com>
2020-09-05 09:57:28 -0400

    Added Info file (necesary for the persistance models)

---
+++ gestal/core/info.py
@@ -0,0 +1,8 @@
+# holds information about the app TODO: delete this
+
+VERSION = '0.01.0'
+# Preferably in lower case and without white space TODO: maybe this should be separated in two variables
+NAME = 'Gestal'
+DESCRIPTION = 'An intuitive task organizer'
+URL = None # TODO
+REPORT_BUG_URL = None # TODO
