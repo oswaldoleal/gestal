6a151e6ef1b460b6673b424de8d8bec58b4b5e63
Oswaldo Leal <ojlr.1998@gmail.com>
2020-09-27 20:36:59 -0400

    Implemented more wrapper functions in backend

--- gestal/backend/backend.py
+++ gestal/backend/backend.py
@@ -1,4 +1,4 @@
-from .models import Project, Task
+from .models import Project, Task, Tag, TagAssignment, Team, TeamPermission, User
 from .persistence import DefaultStorage, Persistence
 
 class Backend:
@@ -8,43 +8,78 @@ class Backend:
     def __init__(self):
         self.persistence = Persistence()
 
+    ## TASK
     # TODO: new_task
+    def new_task(self, name = None, description = None, due_date = None, tags = None):
+        # TODO: properly handle the owner id application wide
+        # TODO: handle the parent_id
+        # TODO: handle the project_id from the currently selected
+        # TODO: handle the tags
+        task = Task(name = name, owner_id = 'oswaldo', parent_id = None, project_id = '1', description = description, status = False, due_date = due_date, persistence = self.persistence)
+        task.save()
 
     # TODO: get_task
 
-    # TODO: get_tasks
+    def get_tasks(self, filter = None):
+        return Task.get_all(Task, self.persistence, filter = filter)
 
+    ## PROJECT
     # TODO: new_project
     def new_project(self, name = None, description = None):
+        # TODO: properly handle the owner id application wide
         project = Project(owner_id = 'oswaldo', name = name, description = description, persistence = self.persistence)
         project.save()
+
     # TODO: get_project
 
-    # TODO: get_projects
+    def get_projects(self, filter = None):
+        return Project.get_all(Project, self.persistence, filter = filter)
 
     # TODO: get_project_progress
 
+    ## TEAM
     # TODO: new_team
 
     # TODO: get_team
 
-    # TODO: get_teams
+    def get_teams(self, filter = None):
+        return Team.get_all(Team, self.persistence, filter = filter)
 
+    ## TEAM PERMISSIONS
+    # TODO: new_team_permission
+
+    # TODO: get_team_permission
+
+    def get_team_permissions(self, filter = None):
+        return TeamPermission.get_all(TeamPermission, self.persistence, filter = filter)
+
+    ## MILESTONE
     # TODO: new_milestone
 
     # TODO: get_milestone
 
     # TODO: get_milestones
 
+    ## TAG
     # TODO: new_tag
 
     # TODO: get_tag
 
-    # TODO: get_tags
+    def get_tags(self, filter = None):
+        return Tag.get_all(Tag, self.persistence, filter = filter)
 
+    ## TAG ASSIGNMENT
+    # TODO: new_tag_assignment
 
-    def get_tasks(self, filter = None):
-        return Task.get_all(Task, self.persistence, filter = filter)
+    # TODO: get_tag_assignment
 
-    def get_projects(self, filter = None):
-        return Project.get_all(Project, self.persistence, filter = filter)
+    def get_tag_assignments(self, filter = None):
+        return TagAssignment.get_all(TagAssignment, self.persistence, filter = filter)
+
+    ## USER
+    # TODO: new_user
+
+    def get_user(self, id = None):
+        return User.get(User, id, self.persistence)
+
+    # TODO: get_users
