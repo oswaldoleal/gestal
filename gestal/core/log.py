94ba2a00c1f6ed0264a75149b92c73591918caaf
Oswaldo Leal <ojlr.1998@gmail.com>
2020-10-02 23:30:28 -0400

    Moved console colors to their own class

--- gestal/core/log.py
+++ gestal/core/log.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import core.config as cfg
 
-class Log():
+class Log:
     pattern = '[{}] [{}] [{}] - {}'
     # INFO - WARNING - ERROR - DEBUG
 
@@ -13,19 +13,26 @@ class Log():
 
     @classmethod
     def info(cls, message, origin = ''):
-        print(cls.get_str(message, origin = origin, type = '\033[1;32;40mINFO\033[0m'))
+        print(cls.get_str(message, origin = origin, type = f'{ConsoleColor.green}INFO{ConsoleColor.end}'))
 
     @classmethod
     def warning(cls, message, origin = ''):
-        print(cls.get_str(message, origin = origin, type = '\033[1;33;40mWARN\033[0m'))
+        print(cls.get_str(message, origin = origin, type = f'{ConsoleColor.yellow}WARN{ConsoleColor.end}'))
 
     @classmethod
     def error(cls, message, origin = ''):
-        print(cls.get_str(message, origin = origin, type = '\033[1;31;40mERRO\033[0m'))
+        print(cls.get_str(message, origin = origin, type = f'{ConsoleColor.red}ERRO{ConsoleColor.end}'))
 
     @classmethod
     def debug(cls, message, origin = '', level = 0):
         if (level <= cfg.VERBOSE):
-            print(cls.get_str(message, origin = origin, type = '\033[1;35;40mDEBU\033[0m'))
+            print(cls.get_str(message, origin = origin, type = f'{ConsoleColor.purple}DEBU{ConsoleColor.end}'))
 
     # TODO: add support to save log to file
+
+class ConsoleColor:
+    green = '\033[1;32;40m'
+    yellow = '\033[1;33;40m'
+    red = '\033[1;31;40m'
+    purple = '\033[1;35;40m'
+    end = '\033[0m'
