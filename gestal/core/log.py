0e1ea5eb0ff39478a46a7e1f0872b12e53ebc4b5
Oswaldo Leal <ojlr.1998@gmail.com>
2020-10-02 23:43:18 -0400

    Saving logs to file. Updated gitignore to exclude log files.

--- gestal/core/log.py
+++ gestal/core/log.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+from os.path import join
 import core.config as cfg
+import core.info as info
 
 class Log:
     pattern = '[{}] [{}] [{}] - {}'
@@ -14,21 +16,28 @@ class Log:
     @classmethod
     def info(cls, message, origin = ''):
         print(cls.get_str(message, origin = origin, type = f'{ConsoleColor.green}INFO{ConsoleColor.end}'))
+        cls.save_to_log(cls.get_str(message, origin = origin, type = 'INFO'))
 
     @classmethod
     def warning(cls, message, origin = ''):
         print(cls.get_str(message, origin = origin, type = f'{ConsoleColor.yellow}WARN{ConsoleColor.end}'))
+        cls.save_to_log(cls.get_str(message, origin = origin, type = '{WARN'))
 
     @classmethod
     def error(cls, message, origin = ''):
         print(cls.get_str(message, origin = origin, type = f'{ConsoleColor.red}ERRO{ConsoleColor.end}'))
+        cls.save_to_log(cls.get_str(message, origin = origin, type = 'ERRO'))
 
     @classmethod
     def debug(cls, message, origin = '', level = 0):
         if (level <= cfg.VERBOSE):
             print(cls.get_str(message, origin = origin, type = f'{ConsoleColor.purple}DEBU{ConsoleColor.end}'))
+            cls.save_to_log(cls.get_str(message, origin = origin, type = 'DEBU'))
 
-    # TODO: add support to save log to file
+    @classmethod
+    def save_to_log(cls, message):
+        with open(join(cfg.LOG_PATH, f'{info.NAME}.log'), 'a') as f:
+            f.write(f'{message}\n')
 
 class ConsoleColor:
     green = '\033[1;32;40m'
